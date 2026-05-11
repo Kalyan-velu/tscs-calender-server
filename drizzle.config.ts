b218c19988db007f44d6727b728aca23d99518cd
Kalyan Jyoti Borah <kalyanborah456@gmail.com>
2026-05-11 23:06:48 +0530

    feat: update database configuration output path and enable strict SSL mode

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,10 +7,10 @@ if (!process.env.DATABASE_URL) {
 
 export default defineConfig({
   schema: './src/db/schema.ts',
-  out: './drizzle',
+  out: './src/db/migrations',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
+    ssl: true
   },
 });
